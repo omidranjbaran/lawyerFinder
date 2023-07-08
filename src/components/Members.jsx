c918d9621b6a69994f8534c19643bf8d95fc5b79
OmidRanjbaran <omidrn58@gmail.com>
2023-07-08 17:47:53 +0330

    improve member section

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,147 +1,50 @@
 import React from 'react'
 import Pexle from '../images/pexels.jpg'
 const Members = () => {
+    const dataMember = [
+        {img:Pexle, name:"احمد حضرت",publishDate:"تاریخ صدور پروانه1397", publishPlace:"مرجع صدور پروانه:مرکز امورمشاوران حقوقی",city:"استان: اصفهان"},
+        {img:Pexle, name:"احمد حضرت",publishDate:"تاریخ صدور پروانه1397", publishPlace:"مرجع صدور پروانه:مرکز امورمشاوران حقوقی",city:"استان: اصفهان"},
+        {img:Pexle, name:"احمد حضرت",publishDate:"تاریخ صدور پروانه1397", publishPlace:"مرجع صدور پروانه:مرکز امورمشاوران حقوقی",city:"استان: اصفهان"},
+        {img:Pexle, name:"احمد حضرت",publishDate:"تاریخ صدور پروانه1397", publishPlace:"مرجع صدور پروانه:مرکز امورمشاوران حقوقی",city:"استان: اصفهان"},
+        {img:Pexle, name:"احمد حضرت",publishDate:"تاریخ صدور پروانه1397", publishPlace:"مرجع صدور پروانه:مرکز امورمشاوران حقوقی",city:"استان: اصفهان"},
+        {img:Pexle, name:"احمد حضرت",publishDate:"تاریخ صدور پروانه1397", publishPlace:"مرجع صدور پروانه:مرکز امورمشاوران حقوقی",city:"استان: اصفهان"},
+    ]
+
+
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 md:pl-[210px] sm:grid-cols-1  gap-6 bg-gradient-to-r from-rose-100 to-teal-100'>
-        <div className='  md:w-[700px]  bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 py-8 flex flex-row justify-center items-center border-4 rounded-3xl  ' >
-            <div>
-                <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={Pexle} alt="" />
-            </div>
-
-            
-            <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
-                <h1 className='text-3xl py-2'>احمد حضرت</h1>
-                تاریخ صدور پروانه:29-07-1390
-                <hr className='mt-2'/>
-                مرجع صدرو پروانه:مرکز امور مشاوران حقوقیه
-                <hr className='mt-2'/>
-                استان:اصفهان
-            </pre>
-        </div>
-
-        <div className='md:w-[700px] bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 py-8 flex flex-row justify-center items-center border-4 rounded-3xl ' >
-            <div>
-                <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={Pexle} alt="" />
-            </div>
-
-            
-            <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
-                <h1 className='text-3xl py-2'>احمد حضرت</h1>
-                تاریخ صدور پروانه:29-07-1390
-                <hr className='mt-2'/>
-                مرجع صدرو پروانه:مرکز امور مشاوران حقوقیه
-                <hr className='mt-2'/>
-                استان:اصفهان
-            </pre>
-        </div>
-
-        <div className='md:w-[700px] bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 py-8 flex flex-row justify-center items-center border-4 rounded-3xl ' >
-            <div>
-                <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={Pexle} alt="" />
-            </div>
-
-            
-            <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
-                <h1 className='text-3xl py-2'>احمد حضرت</h1>
-                تاریخ صدور پروانه:29-07-1390
-                <hr className='mt-2'/>
-                مرجع صدرو پروانه:مرکز امور مشاوران حقوقیه
-                <hr className='mt-2'/>
-                استان:اصفهان
-            </pre>
-        </div>
-
-
-
-        <div className='md:w-[700px] bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 py-8 flex flex-row justify-center items-center border-4 rounded-3xl ' >
-            <div>
-                <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={Pexle} alt="" />
-            </div>
-
-            
-            <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
-                <h1 className='text-3xl py-2'>احمد حضرت</h1>
-                تاریخ صدور پروانه:29-07-1390
-                <hr className='mt-2'/>
-                مرجع صدرو پروانه:مرکز امور مشاوران حقوقیه
-                <hr className='mt-2'/>
-                استان:اصفهان
-            </pre>
-        </div>
-
-        <div className='md:w-[700px] bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 py-8 flex flex-row justify-center items-center border-4 rounded-3xl ' >
-            <div>
-                <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={Pexle} alt="" />
-            </div>
-
-            
-            <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
-                <h1 className='text-3xl py-2'>احمد حضرت</h1>
-                تاریخ صدور پروانه:29-07-1390
-                <hr className='mt-2'/>
-                مرجع صدرو پروانه:مرکز امور مشاوران حقوقیه
-                <hr className='mt-2'/>
-                استان:اصفهان
-            </pre>
-        </div>
-
-
-        <div className='md:w-[700px] bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 py-8 flex flex-row justify-center items-center border-4 rounded-3xl ' >
-            <div>
-                <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={Pexle} alt="" />
-            </div>
-
-            
-            <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
-                <h1 className='text-3xl py-2'>احمد حضرت</h1>
-                تاریخ صدور پروانه:29-07-1390
-                <hr className='mt-2'/>
-                مرجع صدرو پروانه:مرکز امور مشاوران حقوقیه
-                <hr className='mt-2'/>
-                استان:اصفهان
-            </pre>
-        </div>
-
-
-        <div className='md:w-[700px] bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 py-8 flex flex-row justify-center items-center border-4 rounded-3xl ' >
-            <div>
-                <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={Pexle} alt="" />
-            </div>
-
-            
-            <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
-                <h1 className='text-3xl py-2'>احمد حضرت</h1>
-                تاریخ صدور پروانه:29-07-1390
-                <hr className='mt-2'/>
-                مرجع صدرو پروانه:مرکز امور مشاوران حقوقیه
-                <hr className='mt-2'/>
-                استان:اصفهان
-            </pre>
-        </div>
-
-
-
-        <div className='md:w-[700px] bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 py-8 flex flex-row justify-center items-center border-4 rounded-3xl ' >
-            <div>
-                <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={Pexle} alt="" />
-            </div>
-
-            
-            <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
-                <h1 className='text-3xl py-2'>احمد حضرت</h1>
-                تاریخ صدور پروانه:29-07-1390
-                <hr className='mt-2'/>
-                مرجع صدرو پروانه:مرکز امور مشاوران حقوقیه
-                <hr className='mt-2'/>
-                استان:اصفهان
-            </pre>
-        </div>
-
-
-        
-
-    </div>
+    <>
+        <CreateMember {...dataMember[0]}/>
+        <CreateMember {...dataMember[1]}/>
+        <CreateMember {...dataMember[2]}/>
+        <CreateMember {...dataMember[3]}/>
+        <CreateMember {...dataMember[4]}/>
+        <CreateMember {...dataMember[5]}/>
+    </>
   )
+}
+
+
+function CreateMember(props){
+
+    let {img,name,publishDate,publishPlace,city}= props
+    return(
+        <div className='w-[900px]  pl-[60px] py-2 inline-flex gbg-gradient-to-r from-rose-100 to-teal-100'>
+            <div className=' bg-gradient-to-r  from-gray-200 via-gray-400 to-gray-600 py-8  border-4 rounded-3xl  ' >
+                <div className='h-[300px] flex flex-row p-20 gap-3'  > 
+                    <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={img} alt="" />
+                    <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
+                        <h1 className='text-3xl py-2'>{name} </h1>
+                        {publishDate}
+                        <hr className='mt-2'/>
+                        {publishPlace}
+                        <hr className='mt-2'/>
+                        {city}
+                    </pre>
+            
+                </div>        
+            </div>
+    </div>
+    )
 }
 
 export default Members
