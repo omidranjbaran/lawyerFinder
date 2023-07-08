4bef0cd73338072e1c5571030bbc00f6ca7e8fb1
OmidRanjbaran <omidrn58@gmail.com>
2023-07-08 19:47:31 +0330

    solved responsive design for member section

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -28,11 +28,11 @@ function CreateMember(props){
 
     let {img,name,publishDate,publishPlace,city}= props
     return(
-        <div className='w-[900px]  pl-[60px] py-2 inline-flex gbg-gradient-to-r from-rose-100 to-teal-100'>
-            <div className=' bg-gradient-to-r  from-gray-200 via-gray-400 to-gray-600 py-8  border-4 rounded-3xl  ' >
-                <div className='h-[300px] flex flex-row p-20 gap-3'  > 
-                    <img className='w-[130px] rounded-md pr-5 mr-10 h-[160px] ' src={img} alt="" />
-                    <pre className='text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
+        <div className='w-[590px]  sm:w-[700px] md:w-[900px]  pl-[60px] py-2 md:inline-flex gbg-gradient-to-r from-rose-100 to-teal-100'>
+            <div className='ml-[-50px] md:ml-[50px] bg-gradient-to-r  from-gray-200 via-gray-400 to-gray-600 py-8  border-4 rounded-3xl  ' >
+                <div className='h-[300px]  flex flex-row p-20 gap-3'  > 
+                    <img className=' w-[120px] rounded-md pr-5 mr-15 h-[160px] ' src={img} alt="" />
+                    <pre className=' text-sm md:text-xl mr-2 pb-[50px] font-bold text-right'>
                         <h1 className='text-3xl py-2'>{name} </h1>
                         {publishDate}
                         <hr className='mt-2'/>
